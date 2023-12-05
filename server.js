ec61fb24724c90ccef0d134d34560d2eb74cc60d
savan <savannahwilliams243@gmail.com>
2023-12-05 13:24:33 -0800

    import html routes, router, and api routes

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//import the html routes
+const routes = require('./routes');
+//import the router and api routes
+const api = require('./routes/apiRoutes');
