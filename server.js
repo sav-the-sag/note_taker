939a535977251323aa877ff8313b1495b727081a
savan <savannahwilliams243@gmail.com>
2023-12-05 13:26:51 -0800

    added middleware

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ const PORT = process.env.PORT || 3001;
 const routes = require('./routes');
 //import the router and api routes
 const api = require('./routes/apiRoutes');
+
+//add middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
