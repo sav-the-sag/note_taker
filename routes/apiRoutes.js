14199b855512d886f165d70afa5778a081d2b6c1
savan <savannahwilliams243@gmail.com>
2023-12-06 09:53:35 -0800

    get route for saved notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
 // import store class
 const Store = require('../helpers/store');
+
+// GET Route for retrieving all saved notes
+router.get('/notes', async (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    try {
+        //get the saved notes from the db.json file
+        const savedNotes = JSON.parse(await new Store().read());
+        res.json(savedNotes);
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+    });  
