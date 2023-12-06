adfbffbca4cc49c19ae7bd529d3f15b26a43eb19
savan <savannahwilliams243@gmail.com>
2023-12-06 10:05:46 -0800

    define route paths with router.use

--- routes/index.js
+++ routes/index.js
@@ -3,3 +3,8 @@ const router = require('express').Router();
 const htmlRoutes = require('./htmlRoutes');
 const apiRoutes = require('./apiRoutes');
 
+//defining route paths for html and api routes
+router.use('/', htmlRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
