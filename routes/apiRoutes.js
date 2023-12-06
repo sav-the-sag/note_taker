53d70911bd25083924e77285491f98b189434366
savan <savannahwilliams243@gmail.com>
2023-12-06 09:57:01 -0800

    added delete route for deleting note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -35,3 +35,33 @@ router.post('/notes', async (req, res) => {
         res.json('Error in posting note');
     }
 });
+
+// DELETE Route for deleting note
+router.delete('/notes/:note_id', async (req, res) => {
+    try {
+        // Log that a DELETE request was received
+        console.info(`${req.method} request received to delete note`);
+        const deleteNote = (note_id) => {
+            //grabbing the array of notes
+            const savedNotes = JSON.parse(new Store().read());
+            //looping through the array
+            savedNotes.forEach((note, index) => {
+                //if the note id matches the id from the req params, splice it out of the array
+                if (note.note_id === note_id) {
+                    savedNotes.splice(index, 1);
+                }
+            });
+            //write the new array to the db.json file
+            new Store().write(savedNotes);
+            //return the new array
+            return savedNotes;
+        }
+        const savedNotes = await deleteNote(req.params.note_id);
+        res.json(savedNotes);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
