e63c17c874e0e4f9fc8a25aa6a3bbc82f3d7c317
savan <savannahwilliams243@gmail.com>
2023-12-06 09:55:46 -0800

    post route for saving note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,8 +9,29 @@ router.get('/notes', async (req, res) => {
         //get the saved notes from the db.json file
         const savedNotes = JSON.parse(await new Store().read());
         res.json(savedNotes);
-      } catch (err) {
+    } catch (err) {
         console.log(err);
         res.status(500).json(err);
-      }
-    });  
+    }
+});
+
+// POST Route for saving note
+router.post('/notes', async (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to save note`);
+    // Destructuring assignment for the items in req.body
+    if (req.body.title || req.body.text) {
+        //saving newnote obj created in store class addnote method to variable, addnote method writes in db.json file
+        const newNote = new Store();
+        await newNote.addNote(req.body);
+
+        //adding to json file w response obj
+        const response = {
+            status: 'you did it!',
+            body: newNote,
+        }
+        res.json(response);
+    } else {
+        res.json('Error in posting note');
+    }
+});
