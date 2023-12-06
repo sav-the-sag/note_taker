c9534dbb944951688168c5a16796ec49daa48c7c
savan <savannahwilliams243@gmail.com>
2023-12-06 10:02:30 -0800

    get notes w router.get

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,8 @@
 //import express router and path
 const router = require('express').Router();
 const path = require('path');
+
+//GET /notes -> sends note.html file
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'), (err) => err ? console.log(err, 'you broke it no notes') : console.log('you did it'));
+});
